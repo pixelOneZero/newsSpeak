7ffbb6e12a1fa0eca287b7cac0acc9ff78ffea9b
Michael McCarthy <miketmccarthy@gmail.com>
2014-09-22 15:28:12 -0700

    Speak each headline from the news headline service.

--- js/app.js
+++ js/app.js
@@ -15,10 +15,30 @@ var app = {
 			})
 		});
 	},
-	speak: function(words) {
+	speak: function(message) {  
 		if ("SpeechSynthesisEvent" in window) {
+			console.log(message);
+			var msg = new SpeechSynthesisUtterance();
+			var voices = window.speechSynthesis.getVoices();
+			msg.voice = voices[10]; // Note: some voices don't support altering params
+			msg.voiceURI = 'native';
+			msg.volume = 1; // 0 to 1
+			msg.rate = 1; // 0.1 to 10
+			msg.pitch = 1; //0 to 2
+			msg.text = message;
+			msg.lang = 'en-US';
 
+			msg.onend = function(e) {
+			  console.log('Finished in ' + event.elapsedTime + ' seconds.');
+			};
+
+			msg.onerror = function(e) {
+				alert("An error occurred.");
+			}
+
+			speechSynthesis.speak(msg);
 		}
+
 	}
 }
 
