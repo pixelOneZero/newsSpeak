eb6d443fa24030f51fb8b7534000f9687fd9bab0
Michael McCarthy <miketmccarthy@gmail.com>
2014-09-22 14:55:27 -0700

    Add endpoint URI and key to app configuration.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,24 @@ var app = {
 	"name": "newsSpeak",
 	"version": "0.1",
 	"endpoints": {
-		"headlines": "http://api.nytimes.com/svc/news/v3/content"
+		"headlines": {
+			"uri": "http://api.nytimes.com/svc/news/v3/content/",
+			"key": "aaee727aef8a550396b23ee9c7710994:18:63206214",
+			"response-format": "json"
+		}
+	},
+	init: function() {
+		$.getJSON( app.endpoints.headlines.uri + "all/all.jsonp?api-key=" + app.endpoints.headlines.key + "&callback=?", function(data) {
+			$.each(data.results, function(i) {
+				app.speak(data.results[i].title + ": " + data.results[i].abstract);
+			})
+		});
+	},
+	speak: function(words) {
+		if ("SpeechSynthesisEvent" in window) {
+
+		}
 	}
 }
+
+document.onload = app.init();
