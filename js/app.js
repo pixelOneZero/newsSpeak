64f76eb7de0cb186f6338d8a9ace27d4a835be1f
Michael McCarthy <miketmccarthy@gmail.com>
2014-10-01 14:53:47 -0700

    Add upgrade message for non-supported browsers.

--- js/app.js
+++ js/app.js
@@ -16,45 +16,45 @@ var app = {
 		}
 	},
 	init: function() {
-		$.getJSON( app.endpoints.headlines.uri + "all/all.jsonp?api-key=" + app.endpoints.headlines.key + "&callback=?", function(data) {
-			$.each(data.results, function(i) {
-				
-				// Remove special characters so speech API does not utter ampersand, hash sign and number
-				var articleTitle = data.results[i].title;
-				var articleAbstract = data.results[i].abstract;
-				articleAbstract = articleAbstract.replace(/&#8217;/g,"").replace(/&#8220;/g,"").replace(/&#8221;/g,"");
-				app.speak( articleTitle + ": " + articleAbstract);
-			})
-		});
-	},
-	speak: function(message) {  
 		if ("SpeechSynthesisEvent" in window) {
-			$('body').append('<hr/>' + message);
-			var msg = new SpeechSynthesisUtterance();
-			var voices = window.speechSynthesis.getVoices();
-			// Note: some voices don't support altering params
-			msg.voice = voices[10]; 
-			msg.voiceURI = 'native';
-			// 0 to 1
-			msg.volume = 1;
-			// 0.1 to 10
-			msg.rate = 1;
-			//0 to 2
-			msg.pitch = 1; 
-			msg.text = message;
-			msg.lang = 'en-US';
-
-			msg.onend = function(e) {
-			  console.log('Finished in ' + event.elapsedTime + ' seconds.');
-			};
-
-			msg.onerror = function(e) {
-				alert("An error occurred.");
-			}
-
-			speechSynthesis.speak(msg);
+			$.getJSON( app.endpoints.headlines.uri + "all/all.jsonp?api-key=" + app.endpoints.headlines.key + "&callback=?", function(data) {
+				$.each(data.results, function(i) {	
+					// Remove special characters so speech API does not utter ampersand, hash sign and number
+					var articleTitle = data.results[i].title;
+					var articleAbstract = data.results[i].abstract;
+					articleAbstract = articleAbstract.replace(/&#8217;/g,"").replace(/&#8220;/g,"").replace(/&#8221;/g,"");
+					app.speak( articleTitle + ": " + articleAbstract);
+				})
+			});
 		}
+		else {
+			$('body').append('<div class="error">Your browser does not support text-to-speech.  <p><a href="https://www.google.com/chrome/browser/">Please consider upgrading to Google Chrome.</a></strong></div>');
+		}
+	},
+	speak: function(message) {
+		$('body').append('<hr/>' + message);
+		var msg = new SpeechSynthesisUtterance();
+		var voices = window.speechSynthesis.getVoices();
+		// Note: some voices don't support altering params
+		msg.voice = voices[10]; 
+		msg.voiceURI = 'native';
+		// 0 to 1
+		msg.volume = 1;
+		// 0.1 to 10
+		msg.rate = 1;
+		//0 to 2
+		msg.pitch = 1; 
+		msg.text = message;
+		msg.lang = 'en-US';
 
+		msg.onend = function(e) {
+		  console.log('Finished in ' + event.elapsedTime + ' seconds.');
+		};
+
+		msg.onerror = function(e) {
+			alert("An error occurred.");
+		}
+		speechSynthesis.speak(msg);
 	}
 }
 
